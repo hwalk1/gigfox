bb66dc6583db7a0dd87fd5bede948b1816480d43
hayden walker <haydenjwalker1@gmail.com>
2022-06-02 15:38:56 +1000

    Venue create, address picker, cloudinary storage, active storage

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -9,7 +9,7 @@ class VenuesController < ApplicationController
         lat: venue.latitude,
         lng: venue.longitude,
         info_window: render_to_string(partial: "info_window", locals: { venue: venue }),
-        image_url: helpers.asset_url(venue.picture_url)
+        # image_url: helpers.asset_url(venue.picture_url)
       }
     end
   end
@@ -24,16 +24,21 @@ class VenuesController < ApplicationController
         lat: @venue.latitude,
         lng: @venue.longitude,
         info_window: render_to_string(partial: "info_window", locals: { venue: @venue }),
-        image_url: helpers.asset_url(@venue.picture_url)
+        # image_url: helpers.asset_url(@venue.picture_url)
       }]
   end
 
   def new
+    @venue = Venue.new
     authorize @venue
   end
 
   def create
+    @venue = Venue.new(venue_params)
+    @venue.user = current_user
+    @venue.save
     authorize @venue
+    redirect_to venue_path(@venue)
   end
 
   def edit
@@ -46,5 +51,12 @@ class VenuesController < ApplicationController
 
   def destroy
     authorize @venue
+  end
+
+
+  private
+
+  def venue_params
+    params.require(:venue).permit(:name, :description, :location, :price_per_hour, :room_size, :photo) #Says look inside params, look for a key restaurant and permit categories.
   end
 end
